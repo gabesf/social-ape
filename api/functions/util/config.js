8fea53f8735b55d2753aa8b22f880731cef80871
gabesf <gabrielsffair@gmail.com>
2020-02-19 09:20:02 -0300

    first commit?

---
+++ api/functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyBpo7y8Nply9MhGeXeuztNR2HfUZMMiVoI",
+  authDomain: "socialape-a4595.firebaseapp.com",
+  databaseURL: "https://socialape-a4595.firebaseio.com",
+  projectId: "socialape-a4595",
+  storageBucket: "socialape-a4595.appspot.com",
+  messagingSenderId: "701590522523",
+  appId: "1:701590522523:web:09ac5e9c5dac9024f6d43e",
+  measurementId: "G-TLY28C68K8"
+};
